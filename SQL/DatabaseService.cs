b323a88b84dbe624f5003f118c0b99102ea02ac2
ramdaswaghmare <37237891+ramdaswaghmare@users.noreply.github.com>
2018-09-30 09:56:22 +0530

    SQL

---
+++ SQL/DatabaseService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SQL
+{
+	public class DatabaseService : IDatabaseService
+	{
+		private SqlConnection sqlConn;
+		private SqlTransaction sqlTransaction;
+
+		public int ExecuteNonQuery(string sqlProc, SqlParameter[] parameters)
+		{
+			EnsureDatabaseIsOpen();
+
+			// create a command using the procedure, the connection and the optional transaction
+			SqlCommand sqlCommand = CreateCommand(sqlProc, parameters);
+
+			// execute it
+			return sqlCommand.ExecuteNonQuery();
+		}
+
+		public object ExecuteScalar(string sqlProc, SqlParameter[] parameters)
+		{
+			EnsureDatabaseIsOpen();
+
+			// create a command using the procedure, the connection and the optional transaction
+			SqlCommand sqlCommand = CreateCommand(sqlProc, parameters);
+
+			// execute it
+			return sqlCommand.ExecuteScalar();
+		}
+
+		public SqlDataReader ExecuteReader(string sqlProc, SqlParameter[] parameters)
+		{
+			EnsureDatabaseIsOpen();
+
+			// create a command using the procedure, the connection and the optional transaction
+			SqlCommand sqlCommand = CreateCommand(sqlProc, parameters);
+
+			// execute it
+			return sqlCommand.ExecuteReader();
+		}
+
+		private SqlCommand CreateCommand(string sqlProc, SqlParameter[] parameters)
+		{
+			// create a command using the procedure, the connestion and the optional transaction
+			SqlCommand sqlCommand = new SqlCommand(sqlProc, sqlConn, sqlTransaction);
+
+			// we only support Stored Procedures
+			sqlCommand.CommandType = CommandType.StoredProcedure;
+
+			if (parameters != null)
+			{
+				// set null strings to DBNull
+				foreach (SqlParameter parameter in parameters.Where(p => p.SqlDbType == SqlDbType.NVarChar && p.Value == null))
+				{
+					parameter.Value = DBNull.Value;
+				}
+
+				// copy the parameters into the command
+				sqlCommand.Parameters.AddRange(parameters);
+			}
+
+			return sqlCommand;
+		}
+
+		private void EnsureDatabaseIsOpen()
+		{
+			if (sqlConn == null)
+			{
+				OpenDatabase();
+			}
+		}
+
+		private void OpenDatabase()
+		{
+			// this should only be called from EnsureDatabaseIsOpen
+			sqlConn = new SqlConnection();
+			sqlConn.ConnectionString = ConfigurationManager.ConnectionStrings["JBW"].ConnectionString;
+			try
+			{
+				sqlConn.Open();
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+
+	}
+}
---
+++ SQL/IDatabaseService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SQL
+{
+	public interface IDatabaseService
+	{
+		/// <summary>
+		///  Executes a Transact-SQL stored procedure against the database and returns the number of rows affected. 
+		/// </summary>
+		/// <param name="sqlProc">Stored procedure to execute</param>
+		/// <param name="parameters">Array of SqlParameters to use for the procedure. If this is <strong>null</strong> then no parameters are passed.</param>
+		/// <returns>Returns the number of rows affected.</returns>
+		int ExecuteNonQuery(string sqlProc, SqlParameter[] parameters);
+
+		/// <summary>
+		/// Executes a Transact-SQL stored procedure against the database and returns an object representing the result.
+		/// </summary>
+		/// <param name="sqlProc">Stored procedure to execute</param>
+		/// <param name="parameters">Array of SqlParameters to use for the procedure. If this is <strong>null</strong> then no parameters are passed.</param>
+		/// <returns>Returns an object representing the output of the stored procedure.</returns>
+		object ExecuteScalar(string sqlProc, SqlParameter[] parameters);
+
+		/// <summary>
+		/// Executes a Transact-SQL stored procedure against the database and returns a <see cref="SqlDataReader"/> representing the result.
+		/// </summary>
+		/// <param name="sqlProc">Stored procedure to execute</param>
+		/// <param name="parameters">Array of SqlParameters to use for the procedure. If this is <strong>null</strong> then no parameters are passed.</param>
+		/// <returns>Returns a <see cref="SqlDataReader"/> representing the result. The returned <see cref="SqlDataReader"/> uses the 
+		SqlDataReader ExecuteReader(string sqlProc, SqlParameter[] parameters);
+
+		///// Executes a Transact-SQL stored procedure against the database and returns a <see cref="DataSet"/> representing the result.
+		///// </summary>
+		///// <param name="sqlProc">Stored procedure to execute</param>
+		///// <param name="parameters">Array of SqlParameters to use for the procedure. If this is <strong>null</strong> then no parameters are passed.</param>
+		///// <returns>Returns a <see cref="DataSet"/> representing the result. The returned <see cref="DataSet"/> uses the 
+		//DataSet ExecuteDataSet(string sqlProc, SqlParameter[] parameters);
+
+		///// <summary>
+		///// Closes the database connection.
+		///// </summary>
+		//void Close();
+
+	}
+}
